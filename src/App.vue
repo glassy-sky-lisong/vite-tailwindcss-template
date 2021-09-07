2462c1903036217270e7d6f349e86458704df81c
lisong <55542271+glassy-sky-lisong@users.noreply.github.com>
2021-09-07 18:08:11 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Demo from '@/components/Demo.vue'
+</script>
+
+<template>
+  <demo />
+</template>
+
+<style>
+  /*! @import */
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    min-height: 100vh;
+  }
+</style>
